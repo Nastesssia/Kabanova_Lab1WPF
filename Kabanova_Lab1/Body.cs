3475bd489bb71b74d272059739f7430452e4676a
User <User@DESKTOP-C7DGT0E>
2022-02-01 14:54:33 +0200

    Добавьте файлы проекта.

---
+++ Kabanova_Lab1/Body.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kabanova_Lab1
+{
+    public class Body
+    {
+        public double Min { get; set; } = 0;
+        public double Sec { get; set; } = 0;
+        
+        public double Total()
+        {
+            return Min * 60 + Sec;
+        } 
+        public string Print()
+        {
+            return $"Минуты: {Min}, Секунды: {Sec},\n" +
+                   $"Общее количество секунд: {Total()}";
+        }
+    }
+}
